fa20030537af044858c4db8f0bcad0b5730a68fc
Oleo <oleometal@gmail.com>
2018-02-26 14:44:11 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-var five = require("johnny-five");
-var board = new five.Board();
+// Se carga el módulo de HTTP
+var http = require("http");
 
-board.on("ready", function() {
-  var led = new five.Led(13);
-  led.blink(500);
-});
+// Creación del servidor HTTP, y se define la escucha
+// de peticiones en el puerto 8000
+http.createServer(function(request, response) {
+
+   // Se define la cabecera HTTP, con el estado HTTP (OK: 200) y el tipo de contenido
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Se responde, en el cuerpo de la respuesta con el mensaje "Hello World"
+   response.end('Hola Mundo!\n');
+}).listen(8000);
+
+// Se escribe la URL para el acceso al servidor
+console.log('Servidor en la url http://127.0.0.1:8000/');
