432d57adaa2bfa936872be0cc4251dcbd883b126
Oleo <oleometal@gmail.com>
2018-03-15 08:39:01 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-// Se carga el módulo de HTTP
-var http = require("http");
+var express = require('express');
+var app = express();
+var path = require('path');
 
-// Creación del servidor HTTP, y se define la escucha
-// de peticiones en el puerto 8000
-http.createServer(function(request, response) {
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-   // Se define la cabecera HTTP, con el estado HTTP (OK: 200) y el tipo de contenido
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Se responde, en el cuerpo de la respuesta con el mensaje "Hello World"
-   response.end('Hola Mundo!\n');
-}).listen(8000);
-
-// Se escribe la URL para el acceso al servidor
-console.log('Servidor en la url http://127.0.0.1:8000/');
+app.listen(8080);
