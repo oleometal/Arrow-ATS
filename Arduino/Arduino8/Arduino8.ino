777b3ab054ecd456958c9462c1395b9d965b309e
Oleo <oleometal@gmail.com>
2018-05-31 10:41:32 -0600

    Update Arduino8.ino

--- Arduino/Arduino8/Arduino8.ino
+++ Arduino/Arduino8/Arduino8.ino
@@ -63,7 +63,7 @@ void setup() {
   pinMode(8,  INPUT); //<- 8R15 <- 32(UX1W4)
   pinMode(9,  INPUT); //<- 7R15 <- 28(UX1W4)
   pinMode(10, INPUT); //<- 6R15 <- 30(UX1W4)
-  pinMode(11, INPUT); //<- 5R15 <- 26(UX1W4)
+  pinMode(11, INPUT); //<- 5R15 <- UX126(UX1W4)
   pinMode(12, INPUT); //<- 4R15 <- homeUX1W3(UX1W4)
   pinMode(14,OUTPUT); //-> 3R15 -> UX141(UX1W4)
   pinMode(15, INPUT);
