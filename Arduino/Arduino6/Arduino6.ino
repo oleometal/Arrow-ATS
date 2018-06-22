87070d6aae88d24c85127ca363811cd1f29d3055
Oleo <oleometal@gmail.com>
2018-06-22 14:35:11 -0600

    Update Arduino6.ino

--- Arduino/Arduino6/Arduino6.ino
+++ Arduino/Arduino6/Arduino6.ino
@@ -65,7 +65,7 @@ void setup() {
 
   // Listen for incoming connection only from localhost
   // (no one from the external network could connect)
-  server.listenOnLocalhost();
+  //server.listenOnLocalhost();
   server.begin();
 }
 
