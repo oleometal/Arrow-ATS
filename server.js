cd90db3187a1e44ab63b96290e9d3051228e4950
irra <oleometal@gmail.com>
2018-06-27 10:16:05 -0600

    cambio

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.listen(8080);
+//HTTP
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+ var content = '';
+ var type = '';
+ if(req.url === '/') {
+ content = fs.readFileSync('./index.html');
+ type = 'text/html';
+ } else if(req.url === '/styles.css') {
+ content = fs.readFileSync('./styles.css');
+ type = 'text/css';
+ }
+ res.writeHead(200, {'Content-Type': type});
+ res.end(content + '\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
