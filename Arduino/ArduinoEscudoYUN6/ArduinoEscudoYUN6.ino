9948130d14cdc9ef0c85a911c10e7a2c8ea2fef0
Oleo <oleometal@gmail.com>
2018-12-18 10:23:26 -0600

    Update ArduinoEscudoYUN6.ino

--- Arduino/ArduinoEscudoYUN6/ArduinoEscudoYUN6.ino
+++ Arduino/ArduinoEscudoYUN6/ArduinoEscudoYUN6.ino
@@ -41,7 +41,7 @@ void setup() {
   pinMode(3, OUTPUT); //->      -> BLANCO NARANJA (SB132)
   pinMode(4, OUTPUT); //->      -> BLANCO CAFE (SB133)
   pinMode(5, OUTPUT); //->      -> ROJO (SB134)
-  pinMode(6, OUTPUT); //->      -> GRIS
+  pinMode(6, OUTPUT); //->      -> GRIS (SB135)
   pinMode(7, OUTPUT); //->      -> AMARILLO
   pinMode(8, OUTPUT); //->      -> CAFE
   pinMode(9, OUTPUT); //->      -> VERDE
